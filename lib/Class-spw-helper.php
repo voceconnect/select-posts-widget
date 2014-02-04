843c6672ffb2257e437cd0690cfbb779684c5b46
Mat Gargano <mgargano@gmail.com>
2014-02-04 15:21:00 -0500

    Bind javascript events on 'body' instead of '.widget' to ensure event persists after destroying/creating widget, add templating for individual widgets, add indicator if no posts are selected, refactored some WET code, updated code comments

--- lib/Class-spw-helper.php
+++ lib/Class-spw-helper.php
@@ -2,8 +2,19 @@
 
 class Spw_helper {
     
+    /**
+     * Return the post types for this widget
+     *
+     *
+     * Filter(s): 
+     * 'spw_post_type' - filter what post types are included
+     *
+     */
+
     public static function post_types(){
-        return array( 'post' );
+        $default_post_types = array( 'post' );
+        $post_types = apply_filters( 'spw_post_type', $default_post_types );        
+        return $post_types;
     }
 
 }
