9367121c7fc20503718da90c903ae1ba87b5fa1e
Mat Gargano <mgargano@gmail.com>
2014-02-11 16:04:32 -0500

    updating bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@ if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 
 // Add this plugin to WordPress for activation so it can be tested.
 $GLOBALS['wp_tests_options'] = array(
-	'active_plugins' => array( 'post-type-converter/post-type-converter.php' ),
+	'active_plugins' => array( 'select-posts-widget/select-posts-widget.php' ),
 );
 
 require_once __DIR__ . '/pluggable.php';
@@ -12,7 +12,7 @@ require_once __DIR__ . '/pluggable.php';
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-        require dirname( __FILE__ ) . '/../post-type-converter.php';
+        require dirname( __FILE__ ) . '/../select-posts-widget.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
